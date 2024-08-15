71ac50c8e32b9fe2ffd5af9511b5722c2cf75094
Javad Pournosrat <Pournosrat.javad@gmail.com>
2024-08-15 15:20:30 +0330

    cleaning the Mess!

--- app.py
+++ app.py
@@ -1,31 +1,25 @@
 import numpy as np
 import sympy as sp
 
-
+#SYMBOLES
 x, y = sp.symbols('x y')
-#, h = sp.symbols('E h')
-a1, a2, a3, a4, a5, a6, a7, a8, a9 = sp.symbols('a1 a2 a3 a4 a5 a6 a7 a8 a9')
-# = sp.symbols('a')
 
-Lx = 8
-Ly = 8
+#PLATE PROPERTIES
+nu = 0.3
+E = 3e+07
+h = 0.15
+Lx = 4
+Ly = 4
 
-Nex = 4
-Ney = 4
+#NUMBER OF ELEMENTS IN X AND Y DIRECTIONS
+Nex = 1000
+Ney = 1000
 
 a = Lx/Nex
 b = Ly/Ney
-nu = 0.3
-E = 1
-h = 1
 
-w = np.array([[1], [x], [y], [x**2], [x*y], [y**2], [x**3], [(x**2*y + x*y**2)], [y**3]])
 
-alpha = np.array([a1, a2, a3, a4, a5, a6, a7, a8, a9])
-W = alpha@w
-W = W[0]
-
-#Hermitian
+#Hermitian sape functions
 Nx1 = 1-3*(x/a)**2 + 2*(x/a)**3
 Ny1 = 1-3*(y/b)**2 + 2*(y/b)**3
 
@@ -38,6 +32,7 @@ Ny3 = 3*(y/b)**2 - 2*(y/b)**3
 Nx4 = a*( (x/a)**3 - (x/a)**2 )
 Ny4 = b*( (y/b)**3 - (y/b)**2 )
 
+#2D PLATE SHAPE FUNCTIONS
 N1 = Nx1*Ny1
 N2 = Nx1*Ny2
 N3 = Nx2*Ny1
@@ -57,16 +52,15 @@ N16 = Nx2*Ny4
 
 N = np.array([N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, N11, N12, N13, N14, N15, N16])
 
+#second derivative of shape functions with respect to x
 second_diffN_x = []
-
-#second derivative with respect to x
 for i in range(0, len(N)):
     secon_diffN = sp.diff(sp.diff(N[i], x), x)
     #print(secon_diffN)
     second_diffN_x.append(secon_diffN)
 second_diffN_x = np.array(second_diffN_x)
 
-#second derivative with respect to y
+#second derivative of shape functions with respect to y
 second_diffN_y = []
 for j in range(0, len(N)):
     secon_diffN = sp.diff(sp.diff(N[j], y), y)
@@ -74,43 +68,45 @@ for j in range(0, len(N)):
     second_diffN_y.append(secon_diffN)
 second_diffN_y = np.array(second_diffN_y)
 
-#curve
+#drivative of shape functions over x and y
 second_diffN_xy = []
 for k in range(0, len(N)):
-    
     secon_diffNxy = sp.diff(sp.diff(N[k], x), y)
     #print(secon_diffN)
     second_diffN_xy.append(secon_diffNxy)
 second_diffN_xy = np.array(second_diffN_xy)
-#print(second_diffN_xy[0])
-#print(sp.diff((sp.diff(N[0], x)), y))
 
+#D matrix
 d = E*h**3/(12*(1-nu**2))
 D = d*np.array([[1, nu, 0], [nu, 1, 0], [0, 0, (1-nu)/2]])
 
-#B = np.array([second_diffN_x[0], second_diffN_y[0], second_diffN_xy[0]])
 B = []
 for l in range(len(N)):
     BB = np.array([-1*second_diffN_x[l], -1*second_diffN_y[l], -2*second_diffN_xy[l]])
     B.append(BB)
 #B = np.array([B])
+
+#DIRECT INTEGRATION METHOD
 def K(r, s):
     s = int(s)
     r = int(r)
     fk11 = B[r].T@D@B[s] 
     k = sp.integrate(sp.integrate(fk11, (x, 0, a)), (y, 0, b))
-    #print(k11)
     return k
+# diret method
+#K_e1 = np.zeros((16, 16))
+#for o in range(0, 16):
+#    for p in range(0, 16):
+#       K_e1[o, p] = K(o, p)
+#print(K_e1)
 
+
+# NUMERICAL INTEGRATION METHOD
 ww = [ 0.568889, 0.478629, 0.478629, 0.236927, 0.236927 ]
 xx = [ 0, 0.538469, -0.538469, 0.90618, -0.90618 ]
 
-print(K(0, 0))
-
 K_e = np.zeros([16, 16])
 
-#k11 = 0
-# Loop through integration points
 for ii in range(0, 16):
     for jj in range(0, 16):
         fun = B[ii].T @ D @ B[jj]
@@ -126,38 +122,43 @@ for ii in range(0, 16):
                 # Now use the evaluated value in your calculation
                 ke += (a*b/4) * ww[l1] * ww[l2] * fun_evaluated
                 K_e[ii, jj] = ke
-print(K_e)
-        
-
-
-
-
-for o in range(0, 16):
-    for p in range(0, 16):
-       K_e[o, p] = K(o, p)
-#print(K_e)
-
+#print(ke)
 
 #F matrix
 
-p0 = np.array([[25], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]])
+#POINT LOAD
+p0 = np.array([[250], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]])
 
-#F_e = np.zeros((16, 1))
-#F = []
+#DISTRIBUTED LOAD
 
-#p0 = 1
-#for bb in range(0, 16):
-#    N = N.T
-#    #NN = sp.diff(N[bb], y)
-#    f = sp.integrate(sp.integrate(p0*N[bb], (x, 0, a)), (y, 0, b))
-#    F.append(f)
-#
-#for m in range(0, 16):
-#    F_e[m, 0] = F[m]
-#
-#print(F_e)
+F_e = np.zeros((16, 1))
+F = []
+po = 1
 
-#print((K_e**-1)@F_e)
-print((K_e**-1)@p0)
-#F_ee = (p0*a*b)*np.array([1/4, a/24, b/24, (a*b)/14, 1/4, -a/24, b/24, -(a*b)/144, 1/4, -a/24, -b/24, (a*b)/144, 1/4, a/24, -b/24, -(a*b)/144])
+for bb in range(0, 16):
+    N = N.T
+    #NN = sp.diff(N[bb], y)
+    f = sp.integrate(sp.integrate(po*N[bb], (x, 0, a)), (y, 0, b))
+    F.append(f)
+
+for m in range(0, 16):
+    F_e[m, 0] = F[m]
+#F_e = (p0*a*b)*np.array([1/4, a/24, b/24, (a*b)/14, 1/4, -a/24, b/24, -(a*b)/144, 1/4, -a/24, -b/24, (a*b)/144, 1/4, a/24, -b/24, -(a*b)/144])
+
+#print(F_e/((p0*a**2)/(d)))
+delta = (K_e**-1)@p0
+#delta = ((K_e**-1)@F_e)
+#print(delta)
+#print((K_e**-1)@p0)
 #print(F_ee)
+
+w = N@delta
+
+#print(max(delta))
+
+def W_dis(x1, y1):
+    w_evaluated = w[0].subs({x: x1, y: y1}).evalf()
+    print(w_evaluated)
+
+W_dis(2, 2)
+print(delta)
